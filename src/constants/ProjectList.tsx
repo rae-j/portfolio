7aed23e8a55e0784a4619dd963c21ed9ca8df661
siiwinee <siiwinee@gmail.com>
2023-05-27 16:44:09 +1000

    fix: image not showing

--- src/constants/ProjectList.tsx
+++ src/constants/ProjectList.tsx
@@ -11,7 +11,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Greyhound",
     description:
       "Greyhound is known for its unique creativity. The friends did not set out to create a brand but they grew into one.",
-    src: "/assets/images/projects/gh.png",
+    src: "/static/img/gh.png",
     href: "https://www.greyhound.co.th/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -19,7 +19,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Smileyhound",
     description:
       "SMILEYHOUND is a brand under Greyhound portfolio, from small collections enlarging itself to everyday menswear wardrobe staple.",
-    src: "/assets/images/projects/smh.png",
+    src: "/static/img/smh.png",
     href: "https://smileyhound.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -27,7 +27,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Clairis",
     description:
       "Clairis is a culmination of comfort and aesthetics. Our lenses are specially crafted with technological precisions and designed to blend with your eyes colors. ",
-    src: "/assets/images/projects/clairis.png",
+    src: "/static/img/clairis.png",
     href: "https://clairislens.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -35,7 +35,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Jasco",
     description:
       "In 1991, J&P Jasco Products (Thailand) Co., Ltd. was founded as an importer and a distributor in Thailand. Today, our main clients include independent and luxurious hotels, restaurants, national F&B chains, healthcare, industrial food manufacturing, and food service management.",
-    src: "/assets/images/projects/jasco.png",
+    src: "/static/img/jasco.png",
     href: "https://jnpjasco.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -43,7 +43,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Up Only",
     description:
       "UpOnly helps you make data-driven investment choices for play-to-earn and blockchain-based Game Development.",
-    src: "/assets/images/projects/upo.png",
+    src: "/static/img/upo.png",
     href: "https://www.uponly.com/",
     tags: ["React", "Next.js", "Typescript", "Game Development", "Blockchain"]
   },
@@ -51,7 +51,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Nakamoto Games",
     description:
       "Nakamoto Games offers gamers worldwide the opportunity to participate in countless blockchain-based games and generate a sustainable and sizable income.",
-    src: "/assets/images/projects/nakamoto.png",
+    src: "/static/img/nakamoto.png",
     href: "nakamoto.games",
     tags: ["React", "Next.js", "Typescript", "Game Development", "Blockchain"]
   },
@@ -59,7 +59,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Boosey & Hawkes Thailand",
     description:
       "Boosey & Hawkes is home to the largest selection of professional audio equipment and musical instruments, founded in 1969 by KimChang SaeXiao, we’ve remained a family business for more than 50 years and is one of the leading distributors in Thailand.",
-    src: "/assets/images/projects/boosey.png",
+    src: "/static/img/boosey.png",
     href: "https://www.booseythai.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -67,7 +67,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "DM Home",
     description:
       "From urban sleek metropolitan penthouse apartment to rustic, warm rustic luxury second home in the country, DMHOME is your go to leading and integrated design firm for all your design solutions.",
-    src: "/assets/images/projects/dm-e.png",
+    src: "/static/img/dm-e.png",
     href: "https://shop.dm-home.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -75,7 +75,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Lily & Mortimer",
     description:
       "Our mission at Lily & Mortimer is to promote a world where every child can sleep more soundly. Through your purchase of Lily & Mortimer Organic Bedding & Sleepwear we will support programmes that are empowering communities to create a better future for themselves.",
-    src: "/assets/images/projects/lm.png",
+    src: "/static/img/lm.png",
     href: "https://www.lilymortimer.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -83,7 +83,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "One All Sports",
     description:
       "One All Sports was incorporated following 20 years of experience in sports related apparel and accessories industry.",
-    src: "/assets/images/projects/one-all-sport.png",
+    src: "/static/img/one-all-sport.png",
     href: "https://www.oneallsports.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -91,7 +91,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "SmartPro",
     description:
       "Precise and High-Quality Gemological and Jewelry Instruments for the Gems and Jewelry Industry.",
-    src: "/assets/images/projects/smartpro.png",
+    src: "/static/img/smartpro.png",
     href: "https://www.smartproinstrument.com/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -99,7 +99,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Ananta Jewelry",
     description:
       "Ananta is the leading jewelry and diamond dealer with global certification based in Thailand.",
-    src: "/assets/images/projects/ananta.png",
+    src: "/static/img/ananta.png",
     href: "https://anantajewelry.com/",
     tags: ["Magento 2", "E-commerce", "Project Management"]
   },
@@ -107,7 +107,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Amnesty Thailand",
     description:
       "Amnesty International Thailand (also known as Amnesty Thailand and AI Thailand) is a non-governmental organisation focused on the protection of human rights in Thailand and worldwide with more than 1,000 members across Thailand. Amnesty Thailand is one of more than 50 sections that make up Amnesty International worldwide.",
-    src: "/assets/images/projects/amnesty.png",
+    src: "/static/img/amnesty.png",
     href: "https://www.amnesty.or.th/en/",
     tags: ["Concrete5", "CMS", "Web Development", "Project Management"]
   },
@@ -115,7 +115,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Amnesty Shop Thailand",
     description:
       "Amnesty International Thailand (also known as Amnesty Thailand and AI Thailand) is a non-governmental organisation focused on the protection of human rights in Thailand and worldwide with more than 1,000 members across Thailand. Amnesty Thailand is one of more than 50 sections that make up Amnesty International worldwide.",
-    src: "/assets/images/projects/amnesty-e.png",
+    src: "/static/img/amnesty-e.png",
     href: "https://shop.amnesty.or.th/",
     tags: ["Shopify", "E-commerce", "Liquid", "Web Development"]
   },
@@ -123,7 +123,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Black Hat Golf",
     description:
       "An accomplished coach, author and speaker, Thai-American ‘Black Hat’ Tony Meechai got his nickname due to his love for magicians, especially David Copperfield. And, he picked the same black straw hat his childhood golfing idol, Greg Norman, wore during many of his tournament wins!",
-    src: "/assets/images/projects/bhg.png",
+    src: "/static/img/bhg.png",
     href: "https://www.blackhatgolf.com/",
     tags: ["Concrete5", "CMS", "Web Development", "Project Management"]
   },
@@ -131,7 +131,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Chrisko Thailand",
     description:
       "Chrisko develops and exports a variety of products to hotels, restaurants, cafes, retirement/nursing homes, and retail chains through importers all around the world",
-    src: "/assets/images/projects/chrisko.png",
+    src: "/static/img/chrisko.png",
     href: "https://www.chrisko.com/",
     tags: ["Concrete5", "CMS", "Web Development"]
   },
@@ -139,7 +139,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Double A",
     description:
       "Double A is widely recognized as the number one premium paper brand in Asia, with robust performances in Australia, Europe and the Middle East and the potential for strong growth throughout the world. ",
-    src: "/assets/images/projects/double-a.png",
+    src: "/static/img/double-a.png",
     href: "https://www.doubleapaper.com/",
     tags: ["Concrete5", "CMS", "Content Management", "Project Management"]
   },
@@ -147,7 +147,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "BEN TECH CHEMICAL",
     description:
       "BEN TECH CHEMICAL CO. LTD., aim to become a global leader in textile chemicals industry with the best quality products, outstanding service and highest safety.",
-    src: "/assets/images/projects/bentech.png",
+    src: "/static/img/bentech.png",
     href: "https://www.bentechchemical.com/",
     tags: ["WordPress", "Content Management", "Project Management"]
   },
@@ -155,7 +155,7 @@ export const PROJECT_LIST: IProject[] = [
     title: "Adelphi Hospitality",
     description:
       "Adelphi Hospitality Group provides all the amenities you could possibly need for a comfortable home away from home. Whether your stay is short or long, we guarantee it will be a memorable one. Our team member are here to cater to all your needs. At Adelphi, customer satisfaction is our priority.",
-    src: "/assets/images/projects/adelphi.png",
+    src: "/static/img/adelphi.png",
     href: "https://www.adelphihospitality.com/",
     tags: ["Concrete5", "CMS", "Content Management"]
   }
